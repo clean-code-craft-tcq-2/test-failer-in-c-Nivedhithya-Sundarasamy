457d70b643ed162a8a7e5c86008e0669d82c7ed1
Nivedhithya Sundarasamy <Sundarasamy.Nivedhithya@in.bosch.com>
2022-01-22 23:54:04 +0530

    Added assertions to capture the failure usecases
    Signed-off-by: Nivedhithya Sundarasamy <Sundarasamy.Nivedhithya@in.bosch.com>

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,50 @@
 #include <stdio.h>
+
 #include <assert.h>
+#include <string.h>
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+typedef struct {
+	int PairNumber;
+	char MajorColor[10];
+	char MinorColor[10];
+} ColorPair;
+
+void printColorMaponConsole(int d, const char *s1, const char *s2 ) {
+	printf("%d | %s | %s\n", d, s1, s2);
+}
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printColorMaponConsole(i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
 }
 
-int main() {
-    int result = printColorMap();
-    assert(result == 25);
-    printf("All is well (maybe!)\n");
-    return 0;
+ColorPair fakeprintColorMaponConsole(int d, const char *s1, const char *s2){
+	ColorPair colorpair;
+	colorpair.PairNumber = d;
+	strcpy(colorpair.MajorColor, s1);
+	strcpy(colorpair.MinorColor, s2);
+	return colorpair;
 }
+
+int main() {
+   	int result = printColorMap();
+	int i=4, j=3;
+	assert(result == 25);
+	ColorPair colorpair;
+	colorpair = fakeprintColorMaponConsole(i * 5 + j,  majorColor[i], minorColor[i]);
+	printf("Assertion for ColorPairNumber %d\n", i * 5 + j);
+	assert(colorpair.PairNumber == 23);	
+	assert(strcmp(colorpair.MajorColor, "Violet") == 0);
+	assert(strcmp(colorpair.MinorColor, "Green") == 0);
+	printf("All is well (maybe!)\n");
+	return 0;
+}
+
